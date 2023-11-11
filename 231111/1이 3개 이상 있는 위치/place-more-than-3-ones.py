fa9d20d650f420a16edef5706b9313b6c5a98f08
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-12 01:43:42 +0900

    Title: 1이 3개 이상 있는 위치, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231111/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,30 @@
+def in_range(x,y,n):
+    if x<n and y<n:
+        return True
+    else:
+        return False
+
+n = int(input())
+coordinate = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+
+answer = 0
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        point = coordinate[i][j]
+        for dx,dy in zip(dxs,dys):
+            if(in_range(i+dx,j+dy,n)):
+                close_point = coordinate[i+dx][j+dy]
+                if close_point == 1:
+                    cnt += 1
+        
+        if cnt>=3:
+            answer+=1
+
+print(answer)
