d5a7e2c303b8c793c49d992699180d99870ed98f
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-13 00:16:13 +0900

    Title: 되돌아오기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231112/되돌아오기/come-back.py
@@ -0,0 +1,28 @@
+dxs = [1,0,-1,0]
+dys = [0,-1,0,1]
+dir_name = {
+    'E':0,
+    'S':1,
+    'W':2,
+    'N':3
+}
+x,y = 0,0
+
+n = int(input())
+cnt = 1
+answer = -1
+
+for i in range(n):
+    direction, distance = input().split()
+    distance = int(distance)
+    dx = dxs[dir_name[direction]]
+    dy = dys[dir_name[direction]]
+
+    for _ in range(distance):
+        x += dx
+        y += dy
+        if cnt != 1 and x==0 and y==0:
+            answer = cnt
+        else: cnt +=1 
+
+print(answer)
