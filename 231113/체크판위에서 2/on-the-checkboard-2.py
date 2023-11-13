b4e1ced88e3a945fae23944ed002bf27aba2c599
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-13 17:00:17 +0900

    Title: 체크판위에서 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231113/체크판위에서 2/on-the-checkboard-2.py
+++ 231113/체크판위에서 2/on-the-checkboard-2.py
@@ -6,18 +6,20 @@ arr = [
 ]
 
 start,end = arr[0][0], arr[r-1][c-1]
+if(start == end):
+    print(0)
+else:
+    answer = 0
+    for i1 in range(r-2):
+        for j1 in range(c-2):
+            if arr[i1][j1] != start:
+                #print("1 :",i1,j1)
 
-answer = 0
-for i1 in range(r-2):
-    for j1 in range(c-2):
-        if arr[i1][j1] != start:
-            #print("1 :",i1,j1)
+                for i2 in range(i1+1,r-1):
+                    for j2 in range(j1+1,c-1):
+                        if arr[i2][j2] == start: 
+                            #print("2 : ",i2,j2)
+                            answer +=1 
+        
 
-            for i2 in range(i1+1,r-1):
-                for j2 in range(j1+1,c-1):
-                    if arr[i2][j2] == start: 
-                        #print("2 : ",i2,j2)
-                        answer +=1 
-    
-
-print(answer)
+    print(answer)
