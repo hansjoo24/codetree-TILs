fd0bf1cd48db473ce86e781bb06c8528b697182f
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-15 16:44:48 +0900

    Title: 오목, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/오목/O-mok.py
+++ 231115/오목/O-mok.py
@@ -1,3 +1,4 @@
+import sys
 LENGTH = 19
 stones = [
     list(map(int,input().split()))
@@ -17,21 +18,25 @@ for i in range(LENGTH):
         cur_stone_num = stones[i][j]
         if cur_stone_num==0:
             continue
+    
 
         cur_r, cur_c = i,j
         
         for dx,dy in zip(dxs,dys):
-            for _ in range(4):
+            cur_r, cur_c = i,j
+            #print(f"dx,dy = {dx},{dy}")
+            for z in range(4):
                 if in_range(cur_r+dx,cur_c+dy):
                     cur_r,cur_c = cur_r+dx, cur_c+dy
                     if stones[cur_r][cur_c] != cur_stone_num:
                         break
                     else:
                         pass
-                        #print(cur_r,cur_c,dx,dy)
+                        #print(cur_r,cur_c,' : ',z)
 
                 else:
                     break
             else:
                 print(cur_stone_num)
                 print(i+1+2*dx,j+1+2*dy)
+                sys.exit()
