896746d08834e6a6437c9143e605bcdf8d93648b
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-21 14:04:20 +0900

    Title: 아름다운 수열 2, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 231121/아름다운 수열 2/beautiful-sequence-2.py
+++ 231121/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,13 +5,8 @@ cnt=0
 
 for i in range(n-m+1):
     part_a = a[i:i+m]
-    temp_b = b.copy()
-    for num in part_a:
-        if num in temp_b:
-            temp_b.remove(num)
-        else:
-            break
-    else:
+    b.sort()
+    if sorted(part_a)==b:
         cnt+=1
 
 print(cnt)
