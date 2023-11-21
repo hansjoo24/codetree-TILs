612815f95d906a05cce577046d3e6d0ecf6cf5ce
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-21 17:18:48 +0900

    Title: 개발자의 능력 3, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231121/개발자의 능력 3/ability-of-developer-3.py
+++ 231121/개발자의 능력 3/ability-of-developer-3.py
@@ -4,24 +4,16 @@ score2 = 0
 min_score = sys.maxsize
 people = list(map(int,input().split()))
 
+def diff(a,b,c):
+    global people
+    sum1 = people[a]+people[b]+people[c]
+    sum2 = sum(people) - sum1
+
+    return abs(sum1-sum2)
+
 for a in range(6):
     for b in range(6):
-        if b==a:
-            continue
         for c in range(6):
-            if c==a or c==b:
-                continue
-            
-            score1 = 0
-            score2 = 0
+            min_score = min(min_score,diff(a,b,c))
 
-            for i in range(6):
-                if i==a or i==b or i==c:
-                    score1 += people[i]
-                else:
-                    score2 += people[i]
-
-            score = abs(score1-score2)
-            min_score = min(score,min_score)
-            
 print(min_score)
