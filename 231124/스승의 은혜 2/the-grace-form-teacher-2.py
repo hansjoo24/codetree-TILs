a06617716757a5608dcc0783b183e3f10b62406e
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2023-11-24 16:21:49 +0900

    Title: 스승의 은혜 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 231124/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 231124/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -5,6 +5,7 @@ prices = [
     int(input())
     for _ in range(n)
 ]
+prices.sort()
 
 for half in range (n):
     remain = b
@@ -12,13 +13,15 @@ for half in range (n):
     for i in range(n):
         if half == i:
             remain -= int(prices[half]/2)
+            
         else:
             remain -= prices[i]
-            if remain < 0:
-                ans = i
-                break
 
-    if ans == -1:
+        if remain < 0:
+            #print(half,i,prices[i])
+            ans = i
+            break
+    else:
         ans = n
 
     max_ans = max(ans,max_ans)
