4354ce804f73eff64a232d9d6947d4cb0d2aa6b7
hansjoo24 <121699106+hansjoo24@users.noreply.github.com>
2024-05-25 22:05:34 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 165ms, Memory: 26MB, Status: Passed - CodeTree

--- 240525/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240525/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -6,21 +6,23 @@ queue.append((0,0))
 graph = [list(map(int,input().split())) for _ in range(n)]
 visited = [[False]*m for _ in range(n)]
 
-def canGo(x,y):
-    if x<0 or y<0 or x>=m or y>=n:
+
+def canGo(row,col):
+    if row<0 or col<0 or row>=n or col>=m:
         return False
 
-    elif visited[x][y]==True or graph[x][y]==0:
+    elif visited[row][col]==True or graph[row][col]==0:
         return False
     else:
         return True
+
 
 def BFS():
     while len(queue)!= 0:
         x,y = queue[0][0], queue[0][1]
         queue.popleft()
 
-        if x==m-1 and y==n-1:
+        if x==n-1 and y==m-1:
             return 
             
         #print("visited : ",x,y)
@@ -34,7 +36,7 @@ def BFS():
                 visited[newx][newy] = True
 
 BFS()
-if visited[m-1][n-1] == True:
+if visited[n-1][m-1] == True:
     print(1)
 else:
     print(0)
